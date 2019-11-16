5f721e4f9711f16d034d7e5953f589c67142cca7
Kamil <xzwiadowca@gmail.com>
2019-11-16 10:01:19 +0100

    [init]

---
+++ src/main/java/com/example/myspringexample/MySpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspringexample/MySpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
