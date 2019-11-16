ef63823ba58f815dc22f29926cc68ef9b425ad61
Kamil <xzwiadowca@gmail.com>
2019-11-16 11:27:17 +0100

    [add] Controller more endpoints

--- src/main/java/com/example/myspringexample/controller/HelloController.java
+++ src/main/java/com/example/myspringexample/controller/HelloController.java
@@ -21,4 +21,14 @@ public class HelloController {
         modelAndView.setViewName("home");
         return modelAndView;
     }
+    @GetMapping("/tree")
+    ModelAndView myFirstView2(ModelAndView modelAndView, @RequestParam(defaultValue = "Wiola") String name,@RequestParam(defaultValue = "Cegiełka") String surname,@RequestParam(defaultValue = "12") int age){
+        logger.info("name: {}",name);
+
+        modelAndView.addObject("name",name);
+        modelAndView.addObject("surname",surname);
+        modelAndView.addObject("age",age);
+        modelAndView.setViewName("nowa");
+        return modelAndView;
+    }
 }
